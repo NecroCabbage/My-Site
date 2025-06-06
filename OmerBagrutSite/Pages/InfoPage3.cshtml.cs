909099262fe1764b21970b90f5bcd27f3c577742
NecroCabbage <omer.baroz@gmail.com>
2025-06-06 17:05:22 +0300

    sd

---
+++ OmerBagrutSite/Pages/InfoPage3.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OmerBagrutSite.Pages
+{
+    public class InfoPage3Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
